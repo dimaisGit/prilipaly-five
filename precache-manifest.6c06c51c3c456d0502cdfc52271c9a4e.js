e9f94743f8698aa18598dc932fc8ff9395443f4a
Dmitry Semko <dimaisprogrammer@gmail.com>
2019-10-07 14:02:37 +0300

    Updates

---
+++ precache-manifest.6c06c51c3c456d0502cdfc52271c9a4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4b9eeb894cfc88e4831",
+    "url": "/prilipaly-five/static/js/runtime~main.f4b9eeb8.js"
+  },
+  {
+    "revision": "808112a34f25a91b6085",
+    "url": "/prilipaly-five/static/js/main.808112a3.chunk.js"
+  },
+  {
+    "revision": "ffa341f9c8a03c926968",
+    "url": "/prilipaly-five/static/js/2.ffa341f9.chunk.js"
+  },
+  {
+    "revision": "bd4efc2f77bf02e3e58a910bcf4415fc",
+    "url": "/prilipaly-five/index.html"
+  }
+];
