dd6c5015400a75f87c65ee0a8d0616fe08cad3f1
Dmitry Semko <dimaisprogrammer@gmail.com>
2019-10-08 14:01:47 +0300

    fix script

--- src/components/StartComponent/index.js
+++ src/components/StartComponent/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './index.css'
-import {parallax} from '../scripts/parallax'
+import {parallax} from '../../scripts/parallax'
 import CloudsComponent from "../CloudsComponent";
 
 export default class StartComponent extends React.Component {
