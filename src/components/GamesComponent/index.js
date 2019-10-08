40a2c68ae9b37b0a4560d21e760c211339210d27
Dmitry Semko <dimaisprogrammer@gmail.com>
2019-10-08 18:04:51 +0300

    fix script

--- src/components/GamesComponent/index.js
+++ src/components/GamesComponent/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './index.css'
+import CloudsComponent from "../CloudsComponent";
 
 export default class GamesComponent extends React.Component {
     render() {
         return(
-            <div className='gamesWindow'>
+            <div className='gamesWindow Background-bot'>
                 <div className='gamesWindowBg'>
                     <div className='gamesWindowSand'></div>
                     <div className='gamesWindowSky'></div>
+                    <CloudsComponent />
                 </div>
                 <div className='gamesWindowText'>
                     <p className='header'>ИГРАЙ С ПРИЛИПАЛАМИ</p>
