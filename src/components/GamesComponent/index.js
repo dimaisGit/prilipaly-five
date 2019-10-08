82cd705f211d0c2b0186b6aeb93d41b9ca822807
Dmitry Semko <dimaisprogrammer@gmail.com>
2019-10-08 17:53:11 +0300

    fix script

--- src/components/GamesComponent/index.js
+++ src/components/GamesComponent/index.js
@@ -5,7 +5,14 @@ export default class GamesComponent extends React.Component {
     render() {
         return(
             <div className='gamesWindow'>
-                This is the games window
+                <div className='gamesWindowBg'>
+                    <div className='gamesWindowSand'></div>
+                    <div className='gamesWindowSky'></div>
+                </div>
+                <div className='gamesWindowText'>
+                    <p className='header'>ИГРАЙ С ПРИЛИПАЛАМИ</p>
+                    <p className='text-data'>Даже Ворчун не сможет удержаться с такими веселыми играми. Распечатывай, раскрашивай, складывай и играй с друзьями!</p>
+                </div>
             </div>
         )
     }
