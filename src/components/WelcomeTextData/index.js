429355027d3b91992a85ee0c0804e92dedc10594
Александра Чернышева <ach@worldfashion.pro>
2019-10-08 18:55:39 +0300

    added boy and girl

--- src/components/WelcomeTextData/index.js
+++ src/components/WelcomeTextData/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import './index.css'
+import girl from '../../images/girl.png'
 
 export default class GrassComponent extends React.Component {
     render() {
         return (
             <div className="welcomeDataContainer">
                 
+                <div className="logoContainerWelcome ">
+                <div className="logo-d min"></div>
+                    <div className="logo-p ">
+                    </div>
+                    <div className='startWindowClose main-logo shadowViolet' >
+                        ПОЧУДИМ?
+                    </div>
+                </div>
+                <div className="girl">
+                    <img src={girl}></img>
+                </div>
             </div>
         )
     }
