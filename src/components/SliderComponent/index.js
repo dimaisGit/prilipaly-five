e2b0c95ac7b34c8b6e65a0bab1064a5a934faaac
Александра Чернышева <ach@worldfashion.pro>
2019-10-09 17:35:30 +0300

    added slider carcas

--- src/components/SliderComponent/index.js
+++ src/components/SliderComponent/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './index.css'
+import SliderRound from '../RoundedSlider'
 
 export default class SliderComponent extends React.Component {
     render() {
-        return(
+        return (
             <div className='sliderWindow'>
                 <div className="sliderContainer">
-                This is the slider window
+                    <div className="infoBlockContainer">
+                        <div className="infoBlock shadowViolet">
+                            СОБЕРИ ВСЮ КОЛЛЕКЦИЮ ПРИЛИПАЛ!
+                    </div>
+                        <div className="infoBlock red shadowRed">СОВЕРШИ ПОКУПКУ <span className="yellowText">ОТ 500 РУБЛЕЙ</span> И ЗАБЕРИ СВОЕГО ПРИЛИПАЛУ НА КАССЕ</div>
+                    </div>
+                    <SliderRound />
                 </div>
             </div>
         )
