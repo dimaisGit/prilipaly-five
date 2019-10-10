dcbfd2c1a3b945a688bebaf56027a908511edb2a
Александра Чернышева <ach@worldfashion.pro>
2019-10-10 13:14:20 +0300

    added new styles

--- src/components/VideoComponent/index.js
+++ src/components/VideoComponent/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './index.css'
 import phone from '../../images/mobile-phone.png'
+import apple from '../../images/apple-logo.png'
+import googleP from '../../images/google-play.png'
 
 export default class VideoComponent extends React.Component {
     render() {
@@ -8,7 +10,24 @@ export default class VideoComponent extends React.Component {
             <div className='videoWindow'>
                 <div className="videoContainer">
                     <div className="contentBlocks">
-                        <div className="toysAndSocial"></div>
+                        <div className="toysAndSocial">
+                            <div className=""></div>
+                            <div className="socialContainerData">
+                                <div className="infoBlock shadowYellow">
+                                    <span class="blueText">ПРИЛИПАЛЫ В ДОПОЛНЕННОЙ РЕАЛЬНОСТИ!</span>
+                                </div>
+                                <div className="socialBlock">
+                                    <a className="linkToSocial">
+                                        <img src={apple}></img>
+                                    </a>
+                                    <a className="linkToSocial">
+                                        <img src={googleP}></img>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className=""></div>
+                        </div>
                         <div className="videoPhone">
                             <div className="phoneContainer">
                                 <img src={phone}></img>
