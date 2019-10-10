16574829f8ef5621442fd95001e388115a36dae1
Александра Чернышева <ach@worldfashion.pro>
2019-10-10 13:37:08 +0300

    added toys left

--- src/components/VideoComponent/index.js
+++ src/components/VideoComponent/index.js
@@ -3,6 +3,12 @@ import './index.css'
 import phone from '../../images/mobile-phone.png'
 import apple from '../../images/apple-logo.png'
 import googleP from '../../images/google-play.png'
+import rapsberry from '../../images/raspberry1-noacod.png'
+import ufo from '../../images/ufo3_7.png'
+import ufoN from '../../images/ufo3_5.png'
+
+import melon from '../../images/yellowmelon.png'
+
 
 export default class VideoComponent extends React.Component {
     render() {
@@ -11,7 +17,14 @@ export default class VideoComponent extends React.Component {
                 <div className="videoContainer">
                     <div className="contentBlocks">
                         <div className="toysAndSocial">
-                            <div className=""></div>
+                            <div className="toys-1">
+                                <div className="toys-1-1">
+                                    <img src={ufo}></img>
+                                </div>
+                                <div className="toys-1-2">
+                                    <img src={rapsberry}></img>
+                                </div>
+                            </div>
                             <div className="socialContainerData">
                                 <div className="infoBlock shadowYellow">
                                     <span class="blueText">ПРИЛИПАЛЫ В ДОПОЛНЕННОЙ РЕАЛЬНОСТИ!</span>
@@ -26,7 +39,14 @@ export default class VideoComponent extends React.Component {
                                 </div>
                             </div>
 
-                            <div className=""></div>
+                            <div className="toys-2">
+                                <div className="toys-2-1">
+                                    <img src={ufoN}></img>
+                                </div>
+                                <div className="toys-2-2">
+                                    <img src={melon}></img>
+                                </div>
+                            </div>
                         </div>
                         <div className="videoPhone">
                             <div className="phoneContainer">
