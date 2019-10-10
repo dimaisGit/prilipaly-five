2375f8f4f847832fec378bd1418c6c27ee4ff2a4
Александра Чернышева <ach@worldfashion.pro>
2019-10-10 12:26:38 +0300

    added videocontainer

--- src/components/VideoComponent/index.js
+++ src/components/VideoComponent/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './index.css'
+import phone from '../../images/mobile-phone.png'
 
 export default class VideoComponent extends React.Component {
     render() {
         return (
             <div className='videoWindow'>
-                This is the video window
+                <div className="videoContainer">
+                    <div className="contentBlocks">
+                        <div className="toysAndSocial"></div>
+                        <div className="videoPhone">
+                            <div className="phoneContainer">
+                                <img src={phone}></img>
+                            </div>
+                        </div>
+                        <div className="toysAndInfoBlock"></div>
+                    </div>
+                </div>
             </div>
         )
     }
