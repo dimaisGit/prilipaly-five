01da887778f20a6b8d22a3ff6b07d365c28ec59d
Александра Чернышева <ach@worldfashion.pro>
2019-10-10 16:54:34 +0300

    added pic prilipaly

--- src/components/GoodsComponent/index.js
+++ src/components/GoodsComponent/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// import SelectPage from '../../scripts/SelectPage'
 import './index.css'
 
 export default class GoodsComponent extends React.Component {
@@ -6,7 +7,37 @@ export default class GoodsComponent extends React.Component {
         return (
             <div className='goodsWindow'>
                 <div className="goodsContainer">
-                    <div className="wavingBg"></div>
+                    <div className="sponsorsDescription">
+                        <div class="sponsorsInfo">
+                            <h4 class="header smaller lined">ТОВАРЫ-СПОНСОРЫ</h4>
+                            <p class="text-data">Лакомка никогда не упустит шанс съесть чего-нибудь вкусненького. Особенно, если за это можно получить еще больше Прилипал! </p>
+                            <div className="formSearch">
+                                <form>
+                                    <p>
+                                    <label for="region" className="light">Ваш регион:</label>
+                                    <select id="region" name="region">
+                                        <option>ЦФО</option>
+                                        <option>СЗФО</option>
+                                        <option>УФО</option>
+                                    </select>
+                                    <label for=""></label>
+                                    <select className="sponsorsClass">
+                                        <option selected>1 ТОВАР = 1 ПРИЛИПАЛА</option>
+                                        <option>2 ТОВАРА = 1 ПРИЛИПАЛА</option>
+                                        <option>1 ТОВАР = 2 ПРИЛИПАЛЫ</option>
+                                        <option>2 ТОВАРА = 2 ПРИЛИПАЛЫ</option>
+                                    </select>
+                                    </p>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="sponsorsCarousel">
+
+                    </div>
+                </div>
+                <div className="wavingBg">
                 </div>
             </div>
         )
