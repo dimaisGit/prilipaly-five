2618477a18f90efe8dfeb2215198021cd363faf0
Александра Чернышева <ach@worldfashion.pro>
2019-10-14 12:57:41 +0300

    added footer

--- src/components/FooterComponent/index.js
+++ src/components/FooterComponent/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import './index.css'
+import CloudsComponent from "../CloudsComponent";
+import SandComponent from "../SandComponent";
+import GrassComponent from '../GrassComponent'
+
+
 
 export default class FooterComponent extends React.Component {
     render() {
         return (
             <div className='footerWindow'>
-                This is the footer window
+                <div className="footerContainer">
+                    <CloudsComponent />
+                    <GrassComponent />
+                    <SandComponent />
+                    <nav className="footerNavigationPanel">
+                        <ul>
+                            <li><a href="#">КОЛЛЕКЦИЯ</a></li>
+                            <li><a href="#">ПРИЛОЖЕНИЕ-ИГРА</a></li>
+                            <li><a href="#">ТОВАРЫ-СПОНСОРЫ</a></li>
+                            <li><a href="#">КАРТА МАГАЗИНОВ</a></li>
+                            <li><a href="#">ИГРАЙ С ПРИЛИПАЛАМИ</a></li>
+                            <li><a href="#">ОБМЕН ПРИЛИПАЛАМИ</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         )
     }
