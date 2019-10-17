ff90e4f2101fde0fc0fdf0165bf41c64ef2f747d
Александра Чернышева <ach@worldfashion.pro>
2019-10-17 17:36:04 +0300

    added fixed styles

--- src/components/CloudsComponent/index.js
+++ src/components/CloudsComponent/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './index.css'
 
 export default class CloudsComponent extends React.Component {
+
     render() {
         return (
             <div className="cloudsContainer">
