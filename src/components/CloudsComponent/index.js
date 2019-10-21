61998e81a243228d7a1c81f2c0f97ea36dd3b0ea
Александра Чернышева <ach@worldfashion.pro>
2019-10-21 13:00:59 +0300

    fixed clouds

--- src/components/CloudsComponent/index.js
+++ src/components/CloudsComponent/index.js
@@ -2,16 +2,26 @@ import React from 'react'
 import './index.css'
 import { cloudsRunning } from '../../scripts/clouds.js'
 
+// function rect (props) {
+//     const {ctx, x, y, width, height } = props;
+//     ctx.fillRect(x, y, width, height);
+// }
+
+
 export default class CloudsComponent extends React.Component {
     componentDidMount(){
-        cloudsRunning()
+        cloudsRunning();
+    }
+    componentDidUpdate(){
+        cloudsRunning();
+
     }
     render() {
         return (
-            <div className="cloudsContainer " id="clouds-container">
-                <canvas id="clouds-canvas"></canvas>
-                <canvas id="clouds-canvas-2"></canvas>
+            <div className="cloudsContainer">
+                <canvas className="clouds-canvas"></canvas>
             </div>
         )
     }
+
 }
--- src/scripts/clouds.js
+++ src/scripts/clouds.js
@@ -1,82 +1,71 @@
-import cloud from '../images/clouds-1_old.png'
-import cloud2 from '../images/clouds-2.png'
+import cloud from '../images/clouds-1.png'
 
 import $ from 'jquery'
 
 export function cloudsRunning() {
+    let mass = $('.clouds-canvas');
+    for (let i = 0; i < mass.length; i++) {
+        let canvas = $('.clouds-canvas')[i];
+        let ctx = canvas.getContext('2d');
+        let cloudsContainer = $('.cloudsContainer')[i]
 
-    var canvas = $('#clouds-canvas')[0];
-    var ctx = canvas.getContext('2d'); 
-    
-    // Parent to the clouds canvas.
-    let cloudsContainer = $('#clouds-container')
-    
-    // Canvas width and height based on parent.
-    canvas.width = $(cloudsContainer).width(); 
-    canvas.height = $(cloudsContainer).outerHeight();
-    
-    // Create an image element 
-    var img = new Image(); 
-    img.src = cloud;
-    
-    // Global variables
-    var imageWidth;
-    var imageHeight;
-    var stackedVertical;
-    
-    window.onload = function() {
-    
+        canvas.width = $(cloudsContainer).width();
+        canvas.height = $(cloudsContainer).outerHeight();
+        let img = new Image();
+        img.src = cloud;
+
+        // Global letiables
+        let imageWidth;
+        let imageHeight;
+        let stackedVertical;
+
+
         // Update width.
         imageWidth = img.naturalWidth;
         imageHeight = img.naturalHeight;
-    
+
         // Counter
-        var imgCounter = imageWidth; 
-        var scrollSpeed = 2; 
-    
-        // How many stacks of the image do we need.
-        stackedVertical = Math.ceil(canvas.height / imageHeight);
-    
+        let imgCounter = imageWidth;
+        let scrollSpeed = 1.1;
+
+        stackedVertical = 1;
+
         // Loop is called 60FPS.
         function loop() {
             // Clear it on loop.
-            ctx.clearRect(0, 0, canvas.width, canvas.height); 
-    
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
             // We may need this logic more than once.
             for (let i = 0; i < stackedVertical; i++) {
-                    // Draw the image center.
-                    ctx.drawImage(img, imgCounter, (i*imageHeight), imageWidth, imageHeight);
-    
-                    // Draw image forward.
-                    ctx.drawImage(img, imgCounter + imageWidth, (i*imageHeight), imageWidth, imageHeight);
-    
-                    // Draw image behind
-                    ctx.drawImage(img, imgCounter - imageWidth, (i*imageHeight), imageWidth, imageHeight);
+                // Draw the image center.
+                ctx.drawImage(img, imgCounter, (i * imageHeight), imageWidth, imageHeight);
+
+                // Draw image forward.
+                ctx.drawImage(img, imgCounter + imageWidth, (i * imageHeight), imageWidth, imageHeight);
+
+                // Draw image behind
+                ctx.drawImage(img, imgCounter - imageWidth, (i * imageHeight), imageWidth, imageHeight);
             }
-    
+
             // Update image height.
             imgCounter += scrollSpeed;
-    
+
             // Reset
             if (imgCounter >= imageWidth) {
-                    imgCounter = 0; 
+                imgCounter = 0;
             }
-    
-            // Reloop.
-            window.requestAnimationFrame(loop); 
-        } 
-    
-        // Call it once.
-        loop(); 
-    
+
+            window.requestAnimationFrame(loop);
+        }
+        loop();
+
         // On window resize, also resize the canvas.
-        window.addEventListener('resize', function() {
-            canvas.width = $(cloudsContainer).width(); 
+        window.addEventListener('resize', function () {
+            canvas.width = $(cloudsContainer).width();
             canvas.height = $(cloudsContainer).outerHeight();
-    
-            // Reset stacked.
+
             stackedVertical = Math.ceil(canvas.height / imageHeight);
         }, false);
-    
-    };
+    }
+
 }
