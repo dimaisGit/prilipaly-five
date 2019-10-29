9ba2d68b6013a77736f8584212832d9824ebd9ee
Александра Чернышева <ach@worldfashion.pro>
2019-10-29 18:05:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/prilipaly-five/precache-manifest.405a68087a6b2807c5e9fc19bb9b945e.js"
+  "/prilipaly-five/precache-manifest.eb3661faef72711f82e9ac5119311aed.js"
 );
 
 workbox.clientsClaim();
