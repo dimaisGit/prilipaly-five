0e19b4d01cd9a7ef2d350fad0f6e78a6a6aecc63
Александра Чернышева <ach@worldfashion.pro>
2020-09-24 15:11:31 +0300

    Updates

---
+++ precache-manifest.e157ee3e29293f2cf6c0099cb7b252eb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc6d4f9cedc37cdb859f",
+    "url": "/tresnutye/static/css/main.212d43e8.chunk.css"
+  },
+  {
+    "revision": "dc6d4f9cedc37cdb859f",
+    "url": "/tresnutye/static/js/main.7945d41f.chunk.js"
+  },
+  {
+    "revision": "09bad41eb454cadb4c12",
+    "url": "/tresnutye/static/js/runtime~main.452b951c.js"
+  },
+  {
+    "revision": "74c25dc0941a07d5bf8c",
+    "url": "/tresnutye/static/js/2.8172646d.chunk.js"
+  },
+  {
+    "revision": "8373dcab891e3957d42012846e6af9d1",
+    "url": "/tresnutye/static/media/egg5.8373dcab.png"
+  },
+  {
+    "revision": "3bd83cb8df7d2d1aadc8bad30c97c62d",
+    "url": "/tresnutye/static/media/egg.3bd83cb8.png"
+  },
+  {
+    "revision": "08a60201d8f2988c195afc01dfb621df",
+    "url": "/tresnutye/static/media/egg2.08a60201.png"
+  },
+  {
+    "revision": "230708aecc1463ae84d81c6b5c6f30a8",
+    "url": "/tresnutye/static/media/egg3.230708ae.png"
+  },
+  {
+    "revision": "84a5b574545618f09dc914b82786c3cf",
+    "url": "/tresnutye/static/media/egg4.84a5b574.png"
+  },
+  {
+    "revision": "f6016ed6d9c209c7d91b8d82cbf8293b",
+    "url": "/tresnutye/static/media/header.f6016ed6.png"
+  },
+  {
+    "revision": "2af2e4e13169c6121f13acafd5cc91eb",
+    "url": "/tresnutye/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.790a155974f9af9f9d519457c2d0109a.js"
+  "/tresnutye/precache-manifest.e157ee3e29293f2cf6c0099cb7b252eb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tresnutye/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
